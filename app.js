e6078687251cecbe89f2bb4e1bfe324fa507983d
YOusef Hamza <Joseph94h@hotmail.com>
2018-11-06 11:06:45 +0100

    Jag har lagt till felsökning

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-$(document).ready(function() {
-    getQuote();
+$(document).ready(function () {
+    var raknare = 0
+    var antalKlick = 0
+
+    getQuote(); // första
+
     function getQuote() {
-        var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";// här lägger du url till api¨
-       
-        $.getJSON(url, function(data) {
+        raknare += 1
+        console.log("antal hämtningar: " + raknare + " Antal klick: " + antalKlick)
+
+        var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";// här lägger du url till api
+
+        $.getJSON(url, function (data) {
             $("#quote").text('"' + data.quoteText + '"');
             $("#author").text("-" + data.quoteAuthor);
         }); //Stänger .getJSON delen
-
-        $("#newQuote").on("click", function() {
-            getQuote();
-        }); // Stänger klick-funktionen på #newQuote
+        
     };
+    
+
+    $("#newQuote").on("click", function () {
+        antalKlick += 1
+
+        getQuote();
+    }); // Stänger klick-funktionen på #newQuote
 
 });   
